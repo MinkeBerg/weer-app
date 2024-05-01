d2ab762b2ce70d50a28904513c21ab976f04edde
MinkeBerg <167294739+MinkeBerg@users.noreply.github.com>
2024-05-01 19:23:36 +0200

    Added searching functionnality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function refreshTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "39f42bod6b341c40f416ecb0tf021fa4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshTemperature);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = cityInput.value;
+
+  searchCity(cityInput.value);
 }
 
 let formImputElement = document.querySelector("#form-input");
 formImputElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Amsterdam");
